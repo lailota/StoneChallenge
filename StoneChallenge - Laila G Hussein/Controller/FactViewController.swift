127eaf8bb540698eebd5983e537582ca0100d59d
Laila <laila.guzzon@gmail.com>
2021-07-26 12:01:25 -0300

    Fix tableView bugs and insert totalResultsLabel that displays the number of facts to the user.

--- StoneChallenge - Laila G Hussein/Controller/FactViewController.swift
+++ StoneChallenge - Laila G Hussein/Controller/FactViewController.swift
@@ -12,6 +12,7 @@ class FactViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var mySearchBar: UISearchBar!
     @IBOutlet weak var noResultsLabel: UILabel!
+    @IBOutlet weak var totalResultsLabel: UILabel!
     
     var model: FactsManager = {
         return FactsManager.factsManager
@@ -21,6 +22,7 @@ class FactViewController: UIViewController {
         super.viewDidLoad()
         
         noResultsLabel.textColor = .white
+        totalResultsLabel.textColor = .white
         
         self.tableView.dataSource = self
         self.tableView.delegate = self
@@ -38,7 +40,6 @@ class FactViewController: UIViewController {
         } else {
             mySearchBar.isHidden = true
         }
-            
     }
     
 }
@@ -47,24 +48,10 @@ class FactViewController: UIViewController {
 extension FactViewController: UISearchBarDelegate {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
-        model.factsArray.removeAll()
-        
-        if searchBar.isSearchResultsButtonSelected {
-           // self.tableView.backgroundView = nil
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                self.noResultsLabel.textColor = .black
-            }
-            
-        }
+        self.model.totalAct = 0
+        self.model.factsArray.removeAll()
         
         if let realFact = mySearchBar.text {
-            
-            if model.factsArray.isEmpty {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                    self.noResultsLabel.textColor = .black
-                }
-            }
             
             model.getFacts(realFact) { (result) in
                 switch result {
@@ -76,18 +63,30 @@ extension FactViewController: UISearchBarDelegate {
                 }
             }
             let alert = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
-
+            
             let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
             loadingIndicator.hidesWhenStopped = true
             loadingIndicator.style = UIActivityIndicatorView.Style.medium
             loadingIndicator.startAnimating();
-
+            
             alert.view.addSubview(loadingIndicator)
             present(alert, animated: true, completion: nil)
             
             DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
                 self.tableView.reloadData()
                 self.dismiss(animated: false, completion: nil)
+                
+                if self.model.totalAct == 0 {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        self.noResultsLabel.textColor = .black
+                        self.totalResultsLabel.textColor = .white
+                    }
+                } else {
+                    self.noResultsLabel.textColor = .white
+                    self.totalResultsLabel.textColor = .black
+                    self.totalResultsLabel.text = "\(self.model.totalAct) resultados encontrados"
+                }
+                
             }
         }
         searchBar.resignFirstResponder()
@@ -99,29 +98,32 @@ extension FactViewController: UISearchBarDelegate {
 extension FactViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.isUserInteractionEnabled = false
         
         let cell: DisplayFactTableViewCell? = tableView.dequeueReusableCell(withIdentifier: "DisplayFactTableViewCell", for: indexPath) as? DisplayFactTableViewCell
         
+        cell?.backgroundColor = .clear
+        
         if model.factsArray.isEmpty {
-            cell?.backgroundColor = .clear
             return UITableViewCell()
             
         } else {
             
             cell?.setup(value: self.model.factsArray[indexPath.row])
-            
+            tableView.isUserInteractionEnabled = true
             cell?.buttonAction = { sender in
                 let activityController = UIActivityViewController(activityItems: [self.model.factsArray[indexPath.row].url], applicationActivities: nil)
                 self.present(activityController, animated: true, completion: nil)
             }
             return cell ?? UITableViewCell()
         }
- 
+        
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return model.totalAct
+        return model.totalAct
+        
     }
     
     
@@ -129,12 +131,16 @@ extension FactViewController: UITableViewDelegate, UITableViewDataSource {
         performSegue(withIdentifier: "DetailViewController", sender: self)
     }
     
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
         if let destinationVC = segue.destination as? DetailViewController {
+            
             destinationVC.FactDescription = model.factsArray[(tableView.indexPathForSelectedRow?.row)!].value
             destinationVC.urlString = model.factsArray[(tableView.indexPathForSelectedRow?.row)!].url
             tableView.deselectRow(at: tableView.indexPathForSelectedRow!, animated: true)
         }
     }
+    
     
 }
